79a1d552a8c435df70bd444a5b8f9eea3a57ee91
VedantKumarSharma <122880639+VedantKumarSharma@users.noreply.github.com>
2023-01-25 17:19:54 +0530

    abc

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f33c3223-c4b1-46ec-a317-8f8d817c657b</testSuiteGuid>
+</TestSuiteEntity>
